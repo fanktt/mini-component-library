8d93812a7eea0d791e4f89734af2100491762f4b
Fan Kangtai <fankangtai@gmail.com>
2022-11-11 16:01:44 +0800

    Finish ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,54 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react"
+import styled from "styled-components"
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants"
+import VisuallyHidden from "../VisuallyHidden"
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
-};
+  const SIZE = {
+    large: {
+      height: 24,
+      padding: 4,
+      borderRadius: 8,
+    },
+    medium: {
+      height: 12,
+      padding: 0,
+      borderRadius: 4,
+    },
+    small: {
+      height: 8,
+      padding: 0,
+      borderRadius: 4,
+    },
+  }
+  const style = SIZE[size]
+  return (
+    <Wrapper style={style}>
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <Progress
+        role="meter"
+        aria-valuenow={value}
+        aria-valuemin="0"
+        aria-valuemax="100"
+        value={value}
+      ></Progress>
+    </Wrapper>
+  )
+}
 
-export default ProgressBar;
+const Wrapper = styled.div`
+  width: 370px;
+  background: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px hsla(0, 0%, 50%, 0.35);
+`
+
+const Progress = styled.div`
+  background: ${COLORS.primary};
+  width: ${(props) => props.value + "%"};
+  height: 100%;
+  border-radius: ${(props) => (props.value < 100 ? "4px 0 0 4px " : "4px")};
+`
+
+export default ProgressBar
