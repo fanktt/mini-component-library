04e2bd40d5d18eb7ab433062a7032103189563ec
Fan Kangtai <fankangtai@gmail.com>
2022-11-11 18:31:13 +0800

    Finish Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,54 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react"
+import styled from "styled-components"
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants"
+import Icon from "../Icon"
+import { getDisplayedValue } from "./Select.helpers"
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+  const displayedValue = getDisplayedValue(value, children)
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
-};
+    <SelectWrapper>
+      <TextWrapper>{displayedValue}</TextWrapper>
+      <Icon id="chevron-down" strokeWidth="2"></Icon>
+      <RealSelect value={value} onChange={onChange}>
+        {children}
+      </RealSelect>
+    </SelectWrapper>
+  )
+}
 
-export default Select;
+const RealSelect = styled.select`
+  position: absolute;
+  opacity: 0;
+  height: 100%;
+  width: 100%;
+`
+
+const SelectWrapper = styled.div.attrs((props) => ({
+  tabIndex: 0,
+}))`
+  position: relative;
+  height: 43px;
+  background-color: ${COLORS.transparentGray15};
+  width: fit-content;
+  padding: 12px 16px;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const TextWrapper = styled.div`
+  margin-right: 24px;
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  font-size: 1rem;
+`
+
+export default Select
